c14ada0a6d16071b7289734ddc6ecdc366c8b4cc
Dmytro Kodenskyi <dmytrokodenskyi@valtech.com>
2025-01-06 11:22:32 +0200

    app: fix robots

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,20 @@ const nextConfig: NextConfig = {
     deviceSizes: [300, 400, 640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
   },
+  async headers() {
+    return [
+      {
+        // Apply to all routes
+        source: '/:path*',
+        headers: [
+          {
+            key: 'x-robots-tag',
+            value: 'index, follow', // Explicitly allow indexing
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
